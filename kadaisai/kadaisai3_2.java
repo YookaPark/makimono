627cce9cecd13b87ecd189fbe543742ccd798b45
parkyooka <park.yooka@gmail.com>
2021-03-20 15:59:59 +0900

    課題3

---
+++ kadaisai/kadaisai3_2.java
@@ -0,0 +1,12 @@
+package kadaisai;
+
+public class kadaisai3_2 {
+
+	public static void main(String[] args) {
+		int a = 100;
+		double b = 3.1415;
+		
+		System.out.println(a*b + "の型はdoubleです");
+	}
+
+}
