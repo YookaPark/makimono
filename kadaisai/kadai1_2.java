210ad4fc67b6a8b743b9d125621336cc50f243d1
parkyooka <park.yooka@gmail.com>
2021-03-20 15:03:01 +0900

    課題2再提出

---
+++ kadaisai/kadai1_2.java
@@ -0,0 +1,10 @@
+package kadaisai;
+
+public class kadai1_2 {
+
+	public static void main(String[] args) {
+		System.out.println("hello world!!");
+
+	}
+
+}
---
+++ kadaisai/kadai2_2.java
@@ -0,0 +1,22 @@
+package kadaisai;
+
+public class kadai2_2 {
+
+	public static void main(String[] args) {
+		int a = 100 , e = 20;
+		boolean x ,y;
+		x = (a<120);
+		y = (e>0);
+		double b = 100.12;
+		String c = "'100'";
+		String d = "\"100\"";
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(x || y);
+		System.out.println((a==100) && (e==100));
+	}
+
+}
