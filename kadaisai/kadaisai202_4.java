8c3bb50b9fee8c8e05848f381a31c6e92b015062
parkyooka <park.yooka@gmail.com>
2021-04-03 15:36:50 +0900

    課題202_4提出

---
+++ kadaisai/kadaisai202_4.java
@@ -0,0 +1,16 @@
+package kadaisai;
+
+import java.util.Scanner;
+
+public class kadaisai202_4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("あなたの名前を入力してください。");
+		String name = sc.next();
+		System.out.println(name + "さん、こんにちは。");
+
+	}
+
+}
