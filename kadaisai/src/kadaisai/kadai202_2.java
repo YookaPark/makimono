5e87f663e824d19a2af37b1679903d459c9900e2
parkyooka <park.yooka@gmail.com>
2021-04-10 13:55:25 +0900

    4/10　課題202_2 提出

---
+++ kadaisai/src/kadaisai/kadai202_2.java
@@ -0,0 +1,13 @@
+package kadaisai;
+
+public class kadai202_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int banana = 100;
+		int momo = 300;
+		int kazu = 25;
+
+		System.out.println(banana + "円の" + "バナナを"+ kazu + "個、" + momo + "円の桃を" + kazu + "個買ったら、合計"+ (banana * kazu + momo * kazu )+ "円になりました。");
+	}
+}
