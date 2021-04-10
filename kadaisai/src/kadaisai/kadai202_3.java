4a2eb7eeafb29c38b9b5da470bb93399b103cf81
parkyooka <park.yooka@gmail.com>
2021-04-10 15:12:56 +0900

    4/10 課題202_3 提出

---
+++ kadaisai/src/kadaisai/kadai202_3.java
@@ -0,0 +1,19 @@
+package kadaisai; 
+
+public class kadai202_3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String a = "1234567890";
+		int l = a.length();
+		String b = "１２３４５６７８９０";
+		int m = b.length();
+		String c = "飛べない豚は、ただの豚だ。";
+		int n = c.length();
+		
+		System.out.println(l);
+		System.out.println(m);
+		System.out.println(n);
+	}
+
+}
