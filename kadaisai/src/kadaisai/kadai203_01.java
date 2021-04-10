aabaa9e2a4927f173a70da97000c60ef5b52b595
parkyooka <park.yooka@gmail.com>
2021-04-10 15:48:04 +0900

    4/10 課題203_01 提出

---
+++ kadaisai/src/kadaisai/kadai203_01.java
@@ -0,0 +1,16 @@
+package kadaisai;
+
+public class kadai203_01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for(int i = 1;i<=100;i++) {
+				if (i % 7 == 0){
+	                System.out.println(i);
+
+			}
+		}
+
+	}
+
+}
