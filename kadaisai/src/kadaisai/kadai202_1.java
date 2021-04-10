94310927dfb85caa03c4fb4ad51d32f837fd553c
parkyooka <park.yooka@gmail.com>
2021-04-10 11:42:42 +0900

    4/10　課題202_1 提出

---
+++ kadaisai/src/kadaisai/kadai202_1.java
@@ -0,0 +1,15 @@
+package kadaisai;
+
+public class kadai202_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String s1 = "ボク";
+		String s2 = "悪い";
+		String s3 = "スライム";
+		String s4 = "じゃないよ";
+	
+		System.out.println(s1 + s2 + s3 + s4);
+	}
+}
+		
